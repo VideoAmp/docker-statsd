502c86818896a24202007b6348f042c7dad45bf1
Gregory Bean <greg@videoamp.com>
2016-02-01 15:21:16 -0800

    update to use new backend

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@
 
 from    centos:centos7
 run     yum -y update
-run     yum -y install wget git python
+run     yum -y install wget git python gcc gcc-c++ make snappy
 run     wget -O /tmp/node-v5.0.0-linux-x64.tar.gz https://nodejs.org/dist/v5.0.0/node-v5.0.0-linux-x64.tar.gz
 run     tar -C /usr/local/ --strip-components=1 -zxvf /tmp/node-v5.0.0-linux-x64.tar.gz
 run     rm /tmp/node-v5.0.0-linux-x64.tar.gz
 run     git clone git://github.com/etsy/statsd.git statsd
 workdir statsd
-run     npm install git://github.com/markkimsal/statsd-elasticsearch-backend.git
+run     npm install git://github.com/VideoAmp/statsd-kafka-es-backend.git
 workdir /
 add     ./config.js ./statsd/config.js
 
--- config.js
+++ config.js
@@ -1,19 +1,15 @@
 (function() {
-    return {
-        backends: [ './node_modules/statsd-elasticsearch-backend' ],
-        debug: Boolean(process.env.STATSD_DEBUG) || false,
-        port: parseInt(process.env.STATSD_PORT) || 8125,
-        elasticsearch: {
-            port:          9200,
-            host:          "es.prod.use1",
-            path:          "/",
-            indexPrefix:   "statsd",
-            indexTimestamp: "day",
-            countType:     "counter",
-            timerType:     "timer",
-            timerDataType: "timer_data",
-            gaugeDataType: "gauge",
-            formatter:     "default_format"
-        }
-    };
-})()
+  return {
+    backends: [ "./node_modules/statsd-kafka-es-backend" ],
+    debug: Boolean(process.env.STATSD_DEBUG) || false,
+    port: parseInt(process.env.STATSD_PORT) || 8125,
+    kafkaES: {
+      conn:          process.env.KAFKA_CONNECTION || "localhost:2181/",
+      topic:         process.env.KAFKA_TOPIC || "dev.null",
+      countType:     "counter",
+      timerType:     "timer",
+      timerDataType: "timer_data",
+      gaugeDataType: "gauge"
+    }
+  };
+})();
